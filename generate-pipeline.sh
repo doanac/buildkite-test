adc1c886c7c479887c10e7468d5a950dde8973ab
Andy Doan <andy@foundries.io>
2019-07-18 09:55:40 -0500

    rename
    Signed-off-by: Andy Doan <andy@foundries.io>

--- generate-pipeline.sh
+++ generate-pipeline.sh
@@ -6,10 +6,16 @@ cat <<EOF
 steps:
   - command:
     - echo compiling for armhf
+    - ./compile.sh
     agents:
     - "andy-test=true"
+    artifact_paths:
+      - "/archive/*"
   - command:
     - echo compiling for aarch64
+    - ./compile.sh
+    artifact_paths:
+      - "/archive/*"
     agents:
     - "andy-test=true"
 EOF
