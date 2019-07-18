2e1d55230a980e930b8f12c88c3475d8b353ace6
Andy Doan <andy@foundries.io>
2019-07-18 10:08:22 -0500

    use env

--- generate-pipeline.sh
+++ generate-pipeline.sh
@@ -7,11 +7,15 @@ steps:
   - command:
     - echo compiling for armhf
     - ./compile.sh
+    env:
+      BUILD_ARCH=armhf
     agents:
     - "andy-test=true"
   - command:
     - echo compiling for aarch64
     - ./compile.sh
+    env:
+      BUILD_ARCH=aarch64
     agents:
     - "andy-test=true"
 EOF
